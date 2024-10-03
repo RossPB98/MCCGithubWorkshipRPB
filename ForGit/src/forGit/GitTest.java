8bf86a0924459e6b0d805aacc75abd4fd7692ae8
RossPB98 <RossPB@outlook.com>
2024-10-03 15:05:22 -0400

    Update GitTest.java
    Added a for loop to the program

--- ForGit/src/forGit/GitTest.java
+++ ForGit/src/forGit/GitTest.java
@@ -3,5 +3,11 @@ package forGit;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("My name is Ross Barretto"));
+		
+		for (int = 0; i < 5; i++)
+		{
+			System.out.print(i + "  ");
+		}
 	}
 }
